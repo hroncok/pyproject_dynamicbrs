f9d48ebd7773ccddf3b530e0d04dbfd84b3053b6
Miro Hrončok <miro@hroncok.cz>
2019-06-20 10:01:21 +0200

    Parse requirements

--- pyproject_generate_buildrequires.py
+++ pyproject_generate_buildrequires.py
@@ -1,5 +1,13 @@
 import sys
-import pytoml
+
+try:
+    import pytoml
+    from packaging.requirements import Requirement, InvalidRequirement
+    from packaging.utils import canonicalize_name, canonicalize_version
+except ImportError:
+    print("python3dist(pytoml)")
+    print("python3dist(packaging)")
+    sys.exit(0)
 
 try:
     with open("pyproject.toml") as f:
@@ -16,17 +24,61 @@ else:
             pyproject_data["build-system"]["build-backend"]
         )
     except KeyError:
-        import setuptools.build_meta
+        try:
+            import setuptools.build_meta
+        except ImportError:
+            print("python3dist(setuptools) >= 40.8.0")
+            sys.exit(0)
 
         backend = setuptools.build_meta
     except ImportError:
         backend = None
 
+
+requirements = set()
+rpm_requirements = set()
+
+
+def add_requirement(requirement):
+    try:
+        requirements.add(Requirement(requirement))
+    except InvalidRequirement as e:
+        print(
+            f"WARNING: Skipping invalid requirement: {requirement}\n         {e}",
+            file=sys.stderr,
+        )
+
+
 if "requires" in pyproject_data.get("build-system"):
-    print(*pyproject_data["build-system"]["requires"], sep="\n")
+    try:
+        for requirement in pyproject_data["build-system"]["requires"]:
+            add_requirement(requirement)
+    except Exception as e:
+        sys.exit(e)
+
 
 if backend is not None:
     try:
-        print(*backend.get_requires_for_build_wheel(), sep="\n")
+        for requirement in backend.get_requires_for_build_wheel():
+            add_requirement(requirement)
     except Exception as e:
         sys.exit(e)
+
+for requirement in requirements:
+    name = canonicalize_name(requirement.name)
+    together = []
+    for specifier in requirement.specifier:
+        if specifier.operator == "!=":
+            ### XXX < or >
+            together.append(name)
+            continue
+        version = canonicalize_version(specifier.version)
+        together.append(f"python3dist({name}) {specifier.operator} {version}")
+    if len(together) == 0:
+        rpm_requirements.add(f"python3dist({name})")
+    if len(together) == 1:
+        rpm_requirements.add(together[0])
+    elif len(together) > 1:
+        rpm_requirements.add(f"({' and '.join(together)})")
+
+print(*sorted(rpm_requirements), sep="\n")
